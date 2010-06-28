ninowalker/mapnik-trunk

commit a796b7f024006da1148910a77731784cb73e7009

+ use no_skip[] instead of lexeme[] directive   to avoid pre-skipping
git-svn-id: svn+ssh://svn.mapnik.org/trunk@1920 d397654b-2ff0-0310-9e29-ba003691a0f9

include/mapnik/expression_grammar.hpp
@@ -128,7 +128,7 @@ struct expression_grammar : qi::grammar<Iterator, expr_node(), space_type>
         using qi::_a;
         using qi::_b;
         using qi::_r1;
-        using qi::lexeme;
+        using qi::no_skip;
         using qi::_val;
         using qi::lit;
         using qi::int_;
@@ -207,15 +207,13 @@ struct expression_grammar : qi::grammar<Iterator, expr_node(), space_type>
             | int_ [_val = _1]
             | lit("true") [_val = true]
             | lit("false") [_val = false]
-            | '\'' >> ustring [_val = unicode_(_1) ] >> '\''
-            | '[' >> attr [_val = construct<attribute>( _1 ) ] >> ']'
+            | ustring [_val = unicode_(_1) ]
+            | attr [_val = construct<attribute>( _1 ) ]
             | '(' >> expr [_val = _1 ] >> ')'
             ;
         
-        attr %= +(char_ - ']');
-
-        ustring %= lexeme[*(char_-'\'')];
-        
+        attr %= '[' >> +(char_ - ']') >> ']';
+        ustring %= '\'' >> no_skip[+~char_('\'')] >> '\'';
     }
     
     qi::real_parser<double, qi::strict_real_policies<double> > strict_double;

commit 290423f547d07a864e2834b255dccc7a0cc50a9d

+ fixed bug introduced in r1920   (ustring parser includes single quotes    - remove extra '\'' in match/replace)
git-svn-id: svn+ssh://svn.mapnik.org/trunk@1938 d397654b-2ff0-0310-9e29-ba003691a0f9

include/mapnik/expression_grammar.hpp
@@ -166,21 +166,16 @@ struct expression_grammar : qi::grammar<Iterator, expr_node(), space_type>
         
         regex_match_expr = lit(".match")
             >> lit('(') 
-            >> lit('\'')
-            >> ustring [_val = _1]
-            >> lit('\'') 
+            >> ustring [_val = _1] 
             >> lit(')')
             ;
         
         regex_replace_expr = 
             lit(".replace")
             >> lit('(') 
-            >> lit('\'')
             >> ustring           [_a = _1]
-            >> lit('\'') >> lit(',') 
-            >> lit('\'') 
+            >> lit(',') 
             >> ustring           [_b = _1]
-            >> lit('\'')
             >> lit(')')          [_val = regex_replace_(_r1,_a,_b)]
             ;
         